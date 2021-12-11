67f07b495d9cae25440fa316cb6aea48320e6e55
Andrey Filatkin <anfilat@mail.ru>
2021-12-11 12:24:12 +0300

    Check the minimum server version

--- executor.go
+++ executor.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/anfilat/go-ews/internal/errors"
 	"github.com/anfilat/go-ews/internal/ews"
 	"github.com/anfilat/go-ews/internal/requests"
 )
@@ -12,6 +13,12 @@ import (
 func execute(_ context.Context, sd *ews.ServiceData, request requests.Request) (interface{}, error) {
 	if err := sd.Validate(); err != nil {
 		return nil, err
+	}
+	if sd.Version < request.GetMinimumRequiredServerVersion() {
+		return nil, errors.NewValidateError(fmt.Sprintf(
+			"the service request %s is only valid for Exchange version %v or later.",
+			request.GetXmlElementName(),
+			request.GetMinimumRequiredServerVersion()))
 	}
 	if err := request.Validate(); err != nil {
 		return nil, err
--- internal/requests/baseRequest.go
+++ internal/requests/baseRequest.go
@@ -1,6 +1,9 @@
 package requests
 
+import "github.com/anfilat/go-ews/enumerations/exchangeVersion"
+
 type Request interface {
 	Validate() error
 	GetXmlElementName() string
+	GetMinimumRequiredServerVersion() exchangeVersion.Enum
 }
